beaf18afcbbd2dbb9626fb278a891386d7ae36f5
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-05-12 23:36:58 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,21 +7,14 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-display_board(board)
-
-puts "Where would you like to go?"
 
 # code your input_to_index and move method here!def input_to_index(input)
-input = gets
 
-input = ["1","2","3","4","5","6","7","8","9"]
 def input_to_index(input)
   input.to_i - 1
 end
-input_to_index(input)
 
 value = "X"
 def move(board, index, token = "X")
 board[index] = token
 end
-move(board, index, token = "X")
